9aef615c3d43021c771e4ea8574faf1fb767dc64
CarolinsRainbow <carolina__herrera@live.com>
2020-09-20 10:53:55 -0300

    ing

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    horoscopos: []
   },
   mutations: {
+    SET_HOROSCOPOS(state, horoscopos) {
+      state.horoscopos = horoscopos
+    }
   },
   actions: {
+    traerHoroscopos({
+      commit
+    }) {
+      axios.get('https://api.xor.cl/tyaas/').then((data) => {
+        let res = data.data.horoscopo
+        let miDataHoroscopo = Object.values(res)
+        commit('SET_HOROSCOPOS', miDataHoroscopo)
+      })
+    },
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container" id="horoscopo">
+    <div class="row">
+      <div class="col-4" v-for="(horoscopo,index) in horoscopos" :key="index">
+        <b-card
+          no-body
+          style="max-width: 20rem;"
+          img-src="https://placekitten.com/380/200"
+          img-alt="Image"
+          img-top
+        >
+          <template v-slot:header>
+            <h4 class="mb-0"></h4>
+          </template>
+
+          <b-card-body>
+            <b-card-title>{{horoscopo.nombre}}</b-card-title>
+            <b-card-sub-title class="mb-2">{{horoscopo.fechaSigno}}</b-card-sub-title>
+          </b-card-body>
+
+          <b-list-group flush>
+            <b-list-group-item>Amor: {{horoscopo.amor}}</b-list-group-item>
+            <b-list-group-item>Salud: {{horoscopo.salud}}</b-list-group-item>
+            <b-list-group-item>Dinero: {{horoscopo.dinero}}</b-list-group-item>
+          </b-list-group>
+
+          <b-card-img src="https://placekitten.com/480/210" alt="Image" bottom></b-card-img>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "Horoscopo",
+  computed: {
+    ...mapState(["horoscopos"]),
+  },
+  methods: {
+    ...mapActions(["traerHoroscopos"]),
+  },
+  created() {
+    this.traerHoroscopos();
+  },
+};
+</script>
+
